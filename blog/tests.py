a84d5a0071eede421450dc2cbd79c98b6ad45c4b
piet <pprotassow@posteo.de>
2024-09-10 12:02:33 +0200

    CRUD & TESTS

--- blog/tests.py
+++ blog/tests.py
@@ -20,19 +20,24 @@ class BlogTests(TestCase):
             author=cls.user
         )
 
+    def test_post_deleteview(self):
+        response = self.client.post(reverse('post_delete', kwargs={"pk": 1}))
+        self.assertEqual(Post.objects.last(), None) # confirm that no post entries are stored/ or deleted
+        self.assertEqual(response.status_code, 302)
+
     def test_post_model(self):
         self.assertEqual(self.post.title, 'A good title')    
         self.assertEqual(self.post.body, 'Nice body content')    
         self.assertEqual(self.post.author.username, 'testuser')    
         self.assertEqual(str(self.post), 'A good title')    
-        self.assertEqual(self.post.get_absolute_url(), '/post/1')    
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')    
 
     def test_url_exists_at_correct_location_listview(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_url_exists_at_correct_location_detailview(self):
-        response = self.client.get('/post/1')
+        response = self.client.get('/post/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_post_listview(self):
@@ -43,9 +48,39 @@ class BlogTests(TestCase):
     def test_post_listview(self):
         response = self.client.get(reverse('post_detail',
                                 kwargs={'pk': self.post.pk}))
-        no_response = self.client.get('/post/1000')
+        no_response = self.client.get('/post/1000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
+
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse('post_new'),
+            {
+                "title": "new title",
+                "body": "New text",
+                "author": self.user.id,
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "new title")
+        self.assertEqual(Post.objects.last().body, "New text")
+
+    def test_post_updateview(self):
+        response = self.client.post(
+            #reverse("post_edit", args="1"),
+            reverse('post_edit', kwargs={"pk": 1}),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+
+
         
 
     
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import DeleteView, UpdateView, ListView, DetailView, CreateView
 from .models import Post
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -10,9 +10,24 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
+
+    # def get_object(self):
+    #     print(self.kwargs.get('pk'))
+    #     return Post.objects.get(pk=self.kwargs.get('pk'))
+
 class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
 
-    
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("home")
+
